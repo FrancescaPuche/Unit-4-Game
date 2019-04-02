611b890b5ca2e707ef3b027780523ad7025c9cfe
Francesca P <francescapuche@gmail.com>
2019-04-02 17:13:11 -0400

    Added JS and JQuery

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,58 @@
+ // Javascript and JQuery for Crystal Collector Game
+ // ----------------------------------------------------------
+
+
+ // Global Variables 
+ //-----------------------------------------------------------
+var number = Math.floor(Math.random() * (120 - 19 + 1) + 19); // Random number generator between the numbers 19 and 120
+var counter = 0; 
+var targetNumber= number; 
+var wins = 0; 
+var losses = 0; 
+
+
+
+// Functions/Statements/Conditions
+// ----------------------------------------------------------
+
+// Starting Game  
+var start = confirm("Would you like to play Crystal Collector?"); 
+
+if (start === true) { 
+
+    //Assign random number that player number target
+    $("#number").text(targetNumber); 
+
+    // Assigning Numbers to Crystals 
+    function crystalNums () { 
+        $("#blueCrystal").attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1 + 1) + 1));
+        $("#darkGreenCrystal").attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1 + 1) + 1));
+        $("#redCrystal").attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1 + 1) + 1));
+        $("#limeCrystal").attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1 + 1) + 1));
+    }
+
+    $(".crystal").on("click", function() { 
+
+        crystalNums(); 
+
+        var crystalNumber = ($(this).attr("data-crystalvalue")); 
+        crystalNumber = parseInt(crystalNumber); 
+        counter = counter + crystalNumber; 
+
+        $("#totalScore").text(counter);
+
+        if (counter === targetNumber) { 
+            alert("You win!!!"); 
+
+            $("#wins").text(wins = wins + 1);
+             
+        }
+        else if (counter >= targetNumber) { 
+            alert("You lose.")
+
+            $("#losses").text(losses = losses + 1);   
+        }
+    });      
+    
+} 
+
